bebef32109c3332aa9732ebe3f4476b2ac3e76ba
Poisk <vpisotski@gmail.com>
2018-04-27 21:01:03 -0700

    complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,50 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function(value) { //value === {};
+
+    var node = new Node(value);
+
+    if (this.tail === null) {
+      this.head = node;
+      this.tail = node;
+      this.length = 1;
+    } else {
+      if (this.head.next === null) {
+        this.head.next = node;
+      }
+      this.tail.next = node;
+      this.tail = this.tail.next;
+      this.length++;
+    }
   };
 
   list.removeHead = function() {
+    var h = this.head;
+    this.head = this.head.next;
+    this.length;
+    return h.value;
   };
 
   list.contains = function(target) {
+
+    var node = this.head; //null.value === undefined
+    // FIND TARGET IN LIST;
+    var l = this.length;
+    while(l > 0) {
+      if (node === null) {
+        return false; 
+      } else if (node.value === target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+      l--;
+    }
+    return false;
   };
 
+    
   return list;
 };
 
